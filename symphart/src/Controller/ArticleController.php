42917bce54d41cc62362c535746ae9a964dd8204
eliyahukoren <eli.koren@emarsys.com>
2022-08-19 13:06:17 +0300

    Add new action, create new article

--- symphart/src/Controller/ArticleController.php
+++ symphart/src/Controller/ArticleController.php
@@ -6,6 +6,9 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -36,10 +39,42 @@ class ArticleController extends AbstractController {
     }
 
     /**
+     * @Route("/article/new", name="new_article")
+     * @Method({"GET", "POST"})
+     */
+    public function new(Request $request){
+        $article = new Article();
+
+        $form = $this->createFormBuilder(($article))
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array('required' => false, 'attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted() && $form->isValid()){
+            $article = $form->getData();
+
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render(
+            'articles/new.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    /**
      * @Route("/article/{id}", name="article_show")
      * @Method({"GET"})
      */
-    public function show(int $id){
+    public function show($id)
+    {
         $article = $this->doctrine->getRepository(Article::class)->find($id);
 
         return $this->render('articles/show.html.twig', array('article' => $article));
